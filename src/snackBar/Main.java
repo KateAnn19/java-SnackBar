5aa7fa3c393937e56b8728ab529b1db0680f8b71
Kate McGee <mcgeehee@gmail.com>
2020-06-01 20:57:07 -0600

    finished MVP

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -19,43 +19,61 @@ public class Main {
         Snack s_five = new Snack("Water", 20, 2.75, v_drink.getId());
 
         // Processing
-        
-        // Customer 1 (Jane) buys 3 of snack 4 (Soda).  
+
+        // Customer 1 (Jane) buys 3 of snack 4 (Soda).
+        // Print Customer 1 (Jane) Cash on hand.
+        // Print quantity of snack 4 (Soda).
         s_four.buySnack(3);
         double total = s_four.getTotalCost(3);
         c_Jane.buySnacks(total);
-
-        // Print Customer 1 (Jane) Cash on hand.
-        // Print quantity of snack 4 (Soda).
         System.out.println(c_Jane.getCash());
         System.out.println(s_four.getQuantity());
 
-        
         // Customer 1 (Jane) buys 1 of snack 3 (Pretzel).
         // Print Customer 1 (Jane) Cash on hand.
         // Print quantity of snack 3 (Pretzel).
-
+        s_three.buySnack(1);
+        double pretzTotal = s_three.getTotalCost(1);
+        c_Jane.buySnacks(pretzTotal);
+        System.out.println(c_Jane.getCash());
+        System.out.println(s_three.getQuantity());
 
         // Customer 2 (Bob) buys 2 of snack 4 (Soda).
         // Print Customer 2 (Bob) Cash on Hand.
         // Print quantity of snack 4 (Soda).
-
+        s_four.buySnack(2);
+        double sodaTotal = s_four.getTotalCost(2);
+        c_Babby.buySnacks(sodaTotal);
+        System.out.println(c_Babby.getCash());
+        System.out.println(s_four.getQuantity());
 
         // Customer 1 (Jane) finds $10.
         // Print Customer 1 (Jane) Cash on Hand.
-
+        c_Jane.addCash(10);
+        System.out.println(c_Jane.getCash());
 
         // Customer 1 (Jane) buys 1 of snack 2 (Chocolate Bar).
         // Print Customer 1 (Jane) Cash on Hand.
         // Print quantity of snack 2 (Chocolate Bar).
+        s_two.buySnack(1);
+        double chocTotal = s_two.getTotalCost(1);
+        c_Jane.buySnacks(chocTotal);
+        System.out.println(c_Jane.getCash());
+        System.out.println(s_two.getQuantity());
 
         // Add 12 more items to snack 3 (Pretzel).
         // Print quantity of snack 3 (Pretzel).
+        s_three.addQuantity(12);
+        System.out.println(s_three.getQuantity());
 
         // Customer 2 (Bob) buys 3 of snack 3 (Pretzel).
         // Print Customer 2 (Bob) Cash on hand.
         // Print quantity of snack 3 (Pretzel).
-
+        s_three.buySnack(3);
+        double totalThree = s_three.getTotalCost(3);
+        c_Babby.buySnacks(totalThree);
+        System.out.println(c_Babby.getCash());
+        System.out.println(s_three.getQuantity());
 
         // Stretch Goals
 
@@ -64,7 +82,6 @@ public class Main {
         // Vending Machine Name
         // Quantity on hand
         // Total cost of all of the quantities of this snack on hand
-
 
         // Format all dollar amount like $1,234.56
         // Starts with a dollar sign
@@ -76,3 +93,24 @@ public class Main {
         workWithData();
     }
 }
+
+
+
+// Customer 1 cash on hand 37.75
+// Quantity of snack 4 is 21
+
+// Customer 1 cash on hand 35.75
+// Quanity of snack 3 is 29
+
+// Customer 2 cash on hand 28.14
+// Quantity of snack 4 is 19
+
+// Customer 1 cash on hand 45.75
+
+// Customer 1 cash on hand 44.75
+// Quantity of snack 2 is 35
+
+// Quantity of snack 3 is 41
+
+// customer 2 cash on hand 22.14
+// Quantity of snack 3 is 38
